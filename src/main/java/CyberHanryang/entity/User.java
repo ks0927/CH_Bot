a7b682a4771846d5670c98abdfebfdf738af1a0b
ks0927 <brian_als@naver.com>
2023-11-29 23:38:14 +0900

    feat: 유저 엔티티 추가

---
+++ src/main/java/CyberHanryang/entity/User.java
@@ -0,0 +1,22 @@
+package CyberHanryang.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tag;
+
+    private String name;
+}
---
+++ src/main/java/CyberHanryang/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package CyberHanryang.repository;
+
+import CyberHanryang.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
