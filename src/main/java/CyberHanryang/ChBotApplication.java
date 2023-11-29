6a760efb0f3c07fa7fd987e0ff8dc2d9b078c998
ks0927 <brian_als@naver.com>
2023-11-29 22:06:15 +0900

    init commit

---
+++ src/main/java/CyberHanryang/ChBotApplication.java
@@ -0,0 +1,13 @@
+package CyberHanryang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CyberHanryang/ChBotApplicationTests.java
@@ -0,0 +1,13 @@
+package CyberHanryang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
