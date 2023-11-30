1019e514fc16aa865251b815afb533364c8abfd8
ks0927 <brian_als@naver.com>
2023-11-30 16:02:05 +0900

    feat: 서버 엔티티 추가

---
+++ src/main/java/CyberHanryang/entity/Server.java
@@ -0,0 +1,21 @@
+package CyberHanryang.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "servers")
+public class Server {
+    @Id
+    @Column(name = "server_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tag;
+
+    private String name;
+}
