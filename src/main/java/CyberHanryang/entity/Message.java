bc14830c6c0cb522967b517e0568362f9feffc9f
ks0927 <brian_als@naver.com>
2023-11-30 16:51:34 +0900

    fix: 연관관계 추가

--- src/main/java/CyberHanryang/entity/Message.java
+++ src/main/java/CyberHanryang/entity/Message.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,7 +21,11 @@ public class Message {
 
     private String content;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne
+    @JoinColumn(name = "server_id")
     private Server server;
 }
