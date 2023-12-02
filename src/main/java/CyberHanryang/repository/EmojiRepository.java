3ecc37414c6a5a63303e2d104cadbfd9d45df486
ks0927 <brian_als@naver.com>
2023-12-02 16:39:20 +0900

    feat: Server를 키값으로 Emoji 리스트를 반환하는 메서드 추가

--- src/main/java/CyberHanryang/repository/EmojiRepository.java
+++ src/main/java/CyberHanryang/repository/EmojiRepository.java
@@ -1,7 +1,10 @@
 package CyberHanryang.repository;
 
 import CyberHanryang.entity.Emoji;
+import CyberHanryang.entity.Server;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmojiRepository extends JpaRepository<Emoji, Long> {
+    List<Emoji> findByServer(Server server);
 }
